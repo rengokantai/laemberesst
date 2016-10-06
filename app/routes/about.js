d74054e5332501d92ef9c842925c241c35266fe9
Hernan Y.Ke <yuriqiao@gmail.com>
2016-10-05 22:33:45 -0400

    findRecord

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
-let bookmarks =[{
+/*let bookmarks =[{
   id:1,
   title:'js',
   link:'yd.me',
   about:'test'
 }]
-
+*/
 export default Ember.Route.extend({
   model(){
-    return bookmarks;
+    return this.get('store').findRecord('bookmark',1);
   }
 });
